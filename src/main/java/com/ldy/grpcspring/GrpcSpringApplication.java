e783cd05444580146ea5126f2587abdd4abc4692
Daryliu <1201920637@cug.edu.cn>
2021-04-27 11:34:16 +0800

    grpc-java

---
+++ src/main/java/com/ldy/grpcspring/GrpcSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ldy.grpcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ldy/grpcspring/GrpcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldy.grpcspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
